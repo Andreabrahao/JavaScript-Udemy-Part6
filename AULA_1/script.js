8f571dfb2e245f470deec8d5ead232d31ed7b99f
André Abrahão <abrahaoam@gmail.com>
2023-05-09 15:54:18 -0300

    Array e suas funcoes

---
+++ AULA_1/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+let nomes = ['Andre', 'Abrahao', 'Marques'];
+let bool = [true, false, true];
+
+let misturado = [1, 'Andre', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
---
+++ AULA_2/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+let nomes = ['Andre', 'Abrahao', 'Marques'];
+let bool = [true, false, true];
+
+let misturado = [1, 'Andre', true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(misturado[misturado.length - 1]);
